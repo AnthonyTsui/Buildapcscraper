185cbbc0c2d0303f51cae68f4879e700691bf55c
Anthony Tsui <atsui4688@gmail.com>
2019-02-26 22:46:05 -0500

    Testing with newegg, returns titles but not actual prices or links

--- app.js
+++ app.js
@@ -6,17 +6,38 @@ const port = 8000
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('http://google.com', (error, response, html) =>
+//Test newegg link: https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=1080&N=-1&isNodeId=1
+
+//request('http://reddit.com', (error, response, html) =>
+	
+request('https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=1080&N=-1&isNodeId=1', (error, response, html) =>
 {
 	if(!error && response.statusCode == 200) 
 	{
 		//console.log(html);
 
 		const $ = cheerio.load(html);
+		//const siteHeading = $('.site-heading');
 
-		const siteHeading = $('.site-heading');
+		//console.log(html);
 
-		console.log(siteHeading);
+		//const output = siteHeading.find('h1').text();
+		//const output = siteHeading;
+		//const output = $('.page-content'); //returning this as console.log(output.html()) performs as expected
+		const eachResult = $('.item-container'); 
+			//console.log(eachResult.text()) will return the text of all the expected data including button text, image links, descs, titles
+			//const output = eachResult.text(); this also works
+		//const output = eachResult.
+		$('.item-container a').each((i, temp) =>{	//returns names and used(?) prices but will not return actual prices along with link references
+			const item = $(temp).text();
+			console.log(item);
+
+		});
+		
+
+
+		//console.log(output);
+		console.log("------Finished running------");
 	}
 })
 
